36cdf92846fc24878a6c95e5337b2a0913694323
Esad Ali <esadali8653@gmail.com>
2022-12-11 23:05:20 +0530

    done upto 1199px

---
+++ scripts/custom.js
@@ -0,0 +1,58 @@
+
+      $(".hero-slider").owlCarousel({
+        smartSpeed: 500,
+        loop: true,
+        navText: [
+          "<i class='fa-solid fa-arrow-left'></i>",
+          "<i class='fa-solid fa-arrow-right'></i>",
+        ],
+        autoplay: true,
+        dots: false,
+        nav: true,
+        margin: 40,
+        autoplayTimeout: 2500,
+        autoplayHoverPause:true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          1000: {
+            items: 1,
+          },
+        },
+      });
+
+
+
+      // ....... 
+
+      $(".custom-slider").owlCarousel({
+        smartSpeed: 500,
+        loop: true,
+        navText: [
+          "<i class='fa-solid fa-arrow-left'></i>",
+          "<i class='fa-solid fa-arrow-right'></i>",
+        ],
+        autoplay: true,
+        dots: false,
+        nav: true,
+        margin: 40,
+        autoplayTimeout: 2500,
+        autoplayHoverPause:true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          1000: {
+            items: 1,
+          },
+        },
+      });
+
+      
